12bf283e6a225be5664bba29712dbc489f392157
jonatas.araujo <johnlima540@gmail.com>
2025-04-07 11:39:21 -0500

    colocando menu hamburguer

---
+++ js/patins.js
@@ -0,0 +1,11 @@
+function onMenu(){
+    openMenu.style.display = 'none';
+    closeMenu.style.display = 'block'
+    menu.style.display = 'block'
+}
+
+function clMenu(){
+    openMenu.style.display = 'block';
+    closeMenu.style.display = 'none'
+    menu.style.display = 'none'
+}
